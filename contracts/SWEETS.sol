41e8db0ff4a8b563ad130edde2c5fca9a0500e2d
3700 <3700@rove.to>
2022-11-01 07:28:09 +0700

    SWΞΞTS: On-chain Candies

---
+++ contracts/SWEETS.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract SWEETS is ERC721, Ownable, ReentrancyGuard {
+
+        string[] private shapes = [
+                "Pillhead",
+                "Smiler",
+                "Spektral",
+                "Helix",
+                "Tesseract",
+                "Torus",
+                "Obelisk"
+        ];
+
+        string[] private sizes = [
+                "Nibble",
+                "Bite",
+                "Devour"
+        ];
+
+        string[] private surfaces = [
+                "hydro",
+                "plated",
+                "tactile"
+        ];
+
+        string[] private colors = [
+                "#E7434F",
+                "#E7973D",
+                "#E7DC4E",
+                "#5CE75D",
+                "#2981E7", 
+                "#5D21E7", 
+                "#E777E4", 
+                "#E7E7E7", 
+                "#312624",
+                "#E7969F",
+                "#E7B277",
+                "#E7DD8F",
+                "#8CE7C3",
+                "#87B2E7",
+                "#A082E7",
+                "#E4B7E7"
+        ];
+
+        constructor() ERC721 ("SWEETS", "SWEETS") Ownable() {}       
+
+        function mint(uint256 id) public nonReentrant {
+                require(id > 0 && id <= 4500);
+                _safeMint(msg.sender, id);
+        }
+
+        function ownerMint(uint256 id) public nonReentrant onlyOwner {
+                require(id > 4500  && id <= 5000);
+                _safeMint(msg.sender, id);
+        }
+
+        function rand(uint256 id, string memory trait, string[] memory values) internal pure returns (string memory) {
+                uint256 k = uint256(keccak256(abi.encodePacked(trait, toString(id))));
+                return values[k % values.length];
+        }
+
+        function randSize(uint256 id) public view returns (string memory) {
+                return rand(id, "size", sizes);
+        }
+
+        function randSurface(uint256 id) public view returns (string memory) {
+                return rand(id, "shape", sizes);
+        }
+
+        function randShape(uint256 id) public view returns (string memory) {
+                return rand(id, "surface", sizes);
+        }
+
+        function randPalette(uint256 id) public view returns (string[4] memory) {
+                string[4] memory palette;
+                palette[0] = rand(id, "color 0", colors);
+                palette[1] = rand(id, "color 1", colors);
+                palette[2] = rand(id, "color 2", colors);
+                palette[3] = rand(id, "color 3", colors);
+                return palette;
+        }
+
+        function toString(uint256 value) internal pure returns (string memory) {
+                if (value == 0) { return "0"; }
+                uint256 temp = value;
+                uint256 digits;
+                while (temp != 0) {
+                        digits++;
+                        temp /= 10;
+                }
+                bytes memory buffer = new bytes(digits);
+                while (value != 0) {
+                        digits -= 1;
+                        buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+                        value /= 10;
+
+                }
+                return string(buffer);
+        }
+}
